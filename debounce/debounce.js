7826021fcaa7abeb1da218f5ff578612fd9f1ef7
manishkrshukla-myntra <shukla.manish@myntra.com>
2018-10-03 20:41:51 +0530

    added debounce function

---
+++ debounce/debounce.js
@@ -0,0 +1,8 @@
+export default function(funcRef, timeout) {
+    let lastTimeoutRef = null;
+
+    return function() {
+        clearTimeout(lastTimeoutRef);
+        lastTimeoutRef = setTimeout(funcRef.bind(null, arguments), timeout);
+    }
+}
